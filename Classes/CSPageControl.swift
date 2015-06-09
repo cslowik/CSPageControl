3f635844873caa424c66aa48c6951f69e8b1326b
Chris Slowik <chris@creativeda.sh>
2015-06-08 23:37:48 -0700

    added cspagecontrol class file

---
+++ Classes/CSPageControl.swift
@@ -0,0 +1,21 @@
+//
+//  CSPageControl.swift
+//  CSPageControl
+//
+//  Created by Chris Slowik on 6/8/15.
+//  Copyright (c) 2015 Chris Slowik. All rights reserved.
+//
+
+import UIKit
+
+class CSPageControl: UIPageControl {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
